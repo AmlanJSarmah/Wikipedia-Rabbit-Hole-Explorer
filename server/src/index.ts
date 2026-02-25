b07df8062d084b97139efbbba979de3165673a5b
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2026-02-25 14:25:22 +0530

    Global express error handling setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import type {
+  ErrorRequestHandler,
+  Request,
+  Response,
+  NextFunction,
+} from 'express';
 import cors from 'cors';
 // ENV
 import 'dotenv/config';
 import './config/env.js';
 import { env } from './config/env.js';
+// Error
+import { AppError } from './utils/error.js';
+import { ZodError } from 'zod';
 // Router
 import wikipediaRoutes from './routes/wikipedia.routes.js';
 
@@ -14,6 +23,30 @@ app.use(cors());
 
 // Routes
 app.use('/wikipedia', wikipediaRoutes);
+
+// Error Handling
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (err instanceof ZodError) {
+      return res
+        .status(400)
+        .json({ message: 'Validation failed.', error: err });
+    }
+    if (err instanceof AppError) {
+      return res
+        .status(err.statusCode)
+        .json({ message: err.message, error: err });
+    }
+    return res
+      .status(500)
+      .json({ message: 'Internal Server Error', error: err });
+  }
+);
 
 // Start Server
 app.listen(env.PORT, () => {
